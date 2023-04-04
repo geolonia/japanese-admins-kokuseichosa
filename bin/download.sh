49cb1493c4011523bdee04e18507a03ca0f3f7c5
naoki <n.globe.us@gmail.com>
2023-04-04 15:15:38 +0900

    e-statからダウンロードしたshapeファイルを、geojson に変換

--- bin/download.sh
+++ bin/download.sh
@@ -14,6 +14,10 @@ for i in {01..47}; do
   echo "Downloading: $code..."
   curl -o $output_path -L $url
   echo "Downloaded: $code"
+  unzip -o $output_path -d $output_dir/$code
+  ogr2ogr -f GeoJSON $output_dir/$code.geojson $output_dir/$code/r2ka$code.shp
+  rm -rf $output_dir/$code
+  rm $output_dir/$code.zip
 done
 
 echo "All prefecture boundaries downloaded."
