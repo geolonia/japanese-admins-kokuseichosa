196ca377d04272fcc473246e89fdd679d419d272
naoki <n.globe.us@gmail.com>
2023-04-14 17:17:30 +0900

    fix script to merge all geojson

--- bin/download.sh
+++ bin/download.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 
 set -e
-url_template="https://www.e-stat.go.jp/gis/statmap-search/data?dlserveyId=B002005212020&code={code}&coordSys=1&format=shape&downloadType=5&datum=2011"
+
+url_template="https://www.e-stat.go.jp/gis/statmap-search/data?dlserveyId=A002005212020&code={code}&coordSys=1&format=shape&downloadType=5&datum=2011"
 output_dir="data"
-script_dir=$(cd $(dirname $0); pwd)
 
 mkdir -p $output_dir
 
-tail -n +2 $script_dir/admin-code.csv | while read line; do
-  code=$(echo $line | cut -d, -f1)
-  echo $code
+for i in {01..47}; do
 
-  # もし既にダウンロード済みならスキップ
-  if [ -f $output_dir/$code.geojson ]; then
-    echo "Already downloaded: $code"
-    continue
-  fi
-
+  code=$(printf "%02d" $i)
   url=${url_template//\{code\}/$code}
   output_path="$output_dir/$code.zip"
-
   echo "Downloading: $code..."
   curl -o $output_path -L $url
   echo "Downloaded: $code"
+
   unzip -o $output_path -d $output_dir/$code
-
-  baseName=r2kb$code
-  ogr2ogr -f GeoJSON $output_dir/$code.geojson $output_dir/$code/$baseName.shp -dialect sqlite -sql "SELECT ST_Union(geometry), CITY_NAME FROM $baseName GROUP BY CITY_NAME"
-
+  ogr2ogr -f GeoJSON $output_dir/$code.geojson $output_dir/$code/r2ka$code.shp -nln kokuseichosa
   rm -rf $output_dir/$code
   rm $output_dir/$code.zip
+
+  if [ $i -eq "01" ]; then
+    cp $output_dir/$code.geojson $output_dir/all.geojson
+  else
+    ogr2ogr -f GeoJSON -update -append $output_dir/all.geojson $output_dir/$code.geojson -nln kokuseichosa
+  fi
+
+  rm $output_dir/$code.geojson
+
 done
+
+echo "All prefecture boundaries downloaded."
